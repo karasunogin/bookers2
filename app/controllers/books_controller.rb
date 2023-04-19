8549fbece45c9912fb87eac0391453619b9571d2
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-04-19 15:24:22 +0000

    [update]user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,16 +7,31 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user.id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book.params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
